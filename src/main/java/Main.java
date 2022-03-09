6886fa9f1f9655bc640c3cce26e04fd26fbd1c4f
Jahesi <jan.silbernagel@lernende.bbw.ch>
2022-03-09 09:33:15 +0100

    Spielefeldnode klasse fertig. Main mit Todo versehen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Eingabe lesen
+
+        //Spielfeld init
+
+        //Spielfeld struktur 4x4
+
+
+
+        //Pfadmatrix init, speichert array
+
+        //FnW Algo
+
+        //Ausagbe des Pfades in coordinaten der Nodes
     }
 }
